0e0f7634e2b872340565f840b208ead92479a676
Valentine <v.kalu@alustudent.com>
2023-03-10 08:04:16 +0000

    new

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -13,6 +13,6 @@ def number_of_subscribers(subreddit):
 
     if response.status_code == 200:
         data = response.json()
-        return data['data'] ['subscribers']
+        return data['data']['subscribers']
     else:
         return 0
